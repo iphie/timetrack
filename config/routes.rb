17940156e343634990d0ee50bf4d5f4eb9441c04
Morgan <morganschultzdeveloper@gmail.com>
2015-01-29 15:44:51 +0000

    Created some basic layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'pages#home'
+  get 'reports', to: 'pages#reports'
   resources :projects
-  root 'projects#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
