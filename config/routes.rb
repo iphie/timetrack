c3760343cdaea6519bfebae1d8279b715e50369f
Morgan <morganschultzdeveloper@gmail.com>
2015-01-30 14:36:39 +0000

    "branch to postgres"

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
-  get 'reports', to: 'pages#reports'
   resources :projects
   resources :tasks
-
+  get 'reports', to: 'pages#reports'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
